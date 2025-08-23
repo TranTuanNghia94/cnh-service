b868daaca649504246dac3bf0636a13debc755cc
Nghia Tran Tuan <trantuannghia94@gmail.com>
2025-08-23 21:32:55 +0700

    update auth controller

--- src/main/java/com/cnh/ies/controller/AuthController.java
+++ src/main/java/com/cnh/ies/controller/AuthController.java
@@ -1,42 +1,40 @@
-// package com.cnh.ies.controller;
+package com.cnh.ies.controller;
 
-// import com.cnh.ies.dto.common.ApiResponse;
-// import io.swagger.v3.oas.annotations.Operation;
-// import io.swagger.v3.oas.annotations.tags.Tag;
-// import lombok.RequiredArgsConstructor;
-// import lombok.extern.slf4j.Slf4j;
+import java.util.UUID;
 
-// @Slf4j
-// @RestController
-// @RequestMapping("/auth")
-// @RequiredArgsConstructor
-// @Tag(name = "Authentication", description = "Authentication management APIs")
-// public class AuthController {
+import com.cnh.ies.dto.common.ApiResponse;
+import com.cnh.ies.model.auth.LoginModel;
+import com.cnh.ies.model.auth.ResponseLoginModel;
+import com.cnh.ies.service.auth.AuthService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+@Tag(name = "Authentication", description = "Authentication management APIs")
+public class AuthController {
     
-//     // private final UserService userService;
-    
-//     // @PostMapping("/login")
-//     // @Operation(summary = "User login", description = "Authenticate user and return JWT tokens")
-//     // public ResponseEntity<ApiResponse<LoginResponse>> login(@Valid @RequestBody LoginRequest request) {
-//     //     log.info("Login attempt for user: {}", request.getUsername());
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ApiResponse<ResponseLoginModel> login(@RequestBody LoginModel payload) {
+        UUID requestId = UUID.randomUUID();
+        log.info("Login request initiated: {} | RequestId: {}",payload.getUsername(), requestId);
         
-//     //     LoginResponse response = userService.login(request);
+        ResponseLoginModel response = authService.login(payload, null);
+
+        log.info("Login request completed: {} | RequestId: {}",payload.getUsername(), requestId);
         
-//     //     log.info("Login successful for user: {}", request.getUsername());
-//     //     return ResponseEntity.ok(ApiResponse.success(response, "Login successful"));
-//     // }
-    
-//     // @PostMapping("/refresh")
-//     // @Operation(summary = "Refresh token", description = "Refresh access token using refresh token")
-//     // public ResponseEntity<ApiResponse<LoginResponse>> refreshToken(@RequestHeader("Authorization") String refreshToken) {
-//     //     // TODO: Implement refresh token logic
-//     //     return ResponseEntity.ok(ApiResponse.success(null, "Token refreshed successfully"));
-//     // }
-    
-//     // @PostMapping("/logout")
-//     // @Operation(summary = "User logout", description = "Logout user and invalidate tokens")
-//     // public ResponseEntity<ApiResponse<Void>> logout() {
-//     //     // TODO: Implement logout logic (blacklist token)
-//     //     return ResponseEntity.ok(ApiResponse.success(null, "Logout successful"));
-//     // }
-// }
+        return ApiResponse.success(response, "Login success");
+    }
+  
+}
