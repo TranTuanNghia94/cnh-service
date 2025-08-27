468d65ddea2f218a33a7120723796e0414448d4e
Nghia Tran Tuan <trantuannghia94@gmail.com>
2025-08-27 22:52:48 +0700

    create get user and get list users

--- src/main/java/com/cnh/ies/controller/UserController.java
+++ src/main/java/com/cnh/ies/controller/UserController.java
@@ -3,9 +3,12 @@ package com.cnh.ies.controller;
 import java.util.UUID;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cnh.ies.model.general.ListDataModel;
 import com.cnh.ies.dto.common.ApiResponse;
 import com.cnh.ies.model.user.UserInfo;
 import com.cnh.ies.service.user.UserService;
@@ -33,5 +36,29 @@ public class UserController {
         log.info("Getting user info for me success requestId: {}", requestId);
         
         return ApiResponse.success(response, "Get user info success");
+    }
+
+    @GetMapping("/{id}")
+    public ApiResponse<UserInfo> getUserById(@PathVariable String id) {
+        String requestId = UUID.randomUUID().toString();
+        log.info("Getting user info for user id: {} initiated requestId: {}", id, requestId);
+        
+        UserInfo response = userService.getUserById(UUID.fromString(id), requestId);
+
+        log.info("Getting user info for user id: {} success requestId: {}", id, requestId);
+        
+        return ApiResponse.success(response, "Get user info success");
+    }
+
+    @GetMapping("/list")
+    public ApiResponse<ListDataModel<UserInfo>> getUsers(@RequestParam(defaultValue = "1") Integer page, @RequestParam(defaultValue = "10")  Integer limit) {
+        String requestId = UUID.randomUUID().toString();
+        log.info("Getting list users with page: {} and limit: {} initiated requestId: {}", page, limit, requestId);
+        
+        ListDataModel<UserInfo> response = userService.getUsers(requestId, page, limit);
+
+        log.info("Getting list users with page: {} and limit: {} success requestId: {}", page, limit, requestId);
+        
+        return ApiResponse.success(response, "Get list users success");
     }
 }
--- src/main/java/com/cnh/ies/service/user/UserService.java
+++ src/main/java/com/cnh/ies/service/user/UserService.java
@@ -1,12 +1,19 @@
 package com.cnh.ies.service.user;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import com.cnh.ies.entity.auth.UserEntity;
 import com.cnh.ies.exception.ApiException;
+import com.cnh.ies.model.general.ListDataModel;
+import com.cnh.ies.model.general.PaginationModel;
 import com.cnh.ies.model.user.UserInfo;
 import com.cnh.ies.util.RequestContext;
 import com.cnh.ies.repository.auth.UserRepo;
@@ -57,5 +64,56 @@ public class UserService {
         
     }
 
+    public UserInfo getUserById(UUID id, String requestId) {
+        try {
+            Optional<UserEntity> user = userRepo.findById(id);
+
+            if (user.isEmpty()) {
+                log.error("User not found: {} | RequestId: {}", id, requestId);
+                throw new ApiException(ApiException.ErrorCode.NOT_FOUND, "User not found: " + id, HttpStatus.NOT_FOUND.value(), requestId);
+            }
+            
+            if (user.get().getIsDeleted()) {
+                log.error("User is deleted: {} | RequestId: {}", id, requestId);
+                throw new ApiException(ApiException.ErrorCode.NOT_FOUND, "User is deleted: " + id, HttpStatus.NOT_FOUND.value(), requestId);
+            }
+
+            log.info("Getting user info success: {} | RequestId: {}", id, requestId);
+
+            return userMapper.mapToUserInfo(user.get());
+        } catch (Exception e) {
+            log.error("Error getting user info", e);
+            throw new ApiException(ApiException.ErrorCode.INTERNAL_ERROR, e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value(), requestId);
+        }
+    }
     
+
+    public ListDataModel<UserInfo> getUsers(String requestId, Integer page, Integer limit) {
+        try {
+            log.info("Start get list users with page: {} and limit: {} | RequestId: {}", page, limit, requestId);
+
+            Page<UserEntity> users = userRepo.findAll(null, PageRequest.of(page, limit));
+
+            List<UserInfo> userInfos = users.stream()
+                .map(userMapper::mapToUserInfo)
+                .collect(Collectors.toList());
+
+            PaginationModel pagination = PaginationModel.builder()
+                .page(page)
+                .limit(limit)
+                .total(users.getTotalElements())
+                .totalPage(users.getTotalPages())
+                .build();
+
+            log.info("Get list users success with page: {} and limit: {} | RequestId: {}", pagination.getPage(), pagination.getLimit(), requestId);
+
+            return ListDataModel.<UserInfo>builder()
+                .data(userInfos)
+                .pagination(pagination)
+                .build();
+        } catch (Exception e) {
+            log.error("Error getting users", e);
+            throw new ApiException(ApiException.ErrorCode.INTERNAL_ERROR, e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value(), requestId);
+        }
+    }
 }
