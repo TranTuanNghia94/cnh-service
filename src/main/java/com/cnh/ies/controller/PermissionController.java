ad0c7b1559b1d2feaf490599159b0c8a55063d22
Nghia Tran Tuan <trantuannghia94@gmail.com>
2025-09-02 20:32:12 +0700

    create crud for permission

--- src/main/java/com/cnh/ies/controller/PermissionController.java
+++ src/main/java/com/cnh/ies/controller/PermissionController.java
@@ -5,12 +5,16 @@ import java.util.UUID;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import com.cnh.ies.dto.common.ApiResponse;
+import com.cnh.ies.model.general.GeneralRequest;
 import com.cnh.ies.model.general.ListDataModel;
 import com.cnh.ies.model.user.PermissionInfo;
 import com.cnh.ies.service.user.PermissionService;
@@ -21,7 +25,7 @@ import com.cnh.ies.service.user.PermissionService;
 @Slf4j
 @Tag(name = "Permission", description = "Permission management APIs")
 public class PermissionController {
-    
+
     private final PermissionService permissionService;
 
     @GetMapping("/list")
@@ -32,7 +36,46 @@ public class PermissionController {
         ListDataModel<PermissionInfo> response = permissionService.getAllPermissions(requestId);
 
         log.info("Getting all permissions success with requestId: {}", requestId);
-        
+
         return ApiResponse.success(response, "Get all permissions success");
     }
+
+    @GetMapping("/{id}")
+    public ApiResponse<PermissionInfo> getPermissionById(@PathVariable String id) {
+        String requestId = UUID.randomUUID().toString();
+        log.info("Getting permission by id with initiated requestId: {}", requestId);
+
+        PermissionInfo response = permissionService.getPermissionById(id, requestId);
+
+        log.info("Getting permission by id success with requestId: {}", requestId);
+
+        return ApiResponse.success(response, "Get permission by id success");
+
+    }
+
+    @PostMapping("/delete")
+    public ApiResponse<String> deletePermission(@RequestBody GeneralRequest request) {
+        String requestId = UUID.randomUUID().toString();
+        log.info("Deleting permission with initiated requestId: {}", requestId);
+
+        String response = permissionService.deletePermission(request.getRequestId(), requestId);
+
+        log.info("Deleting permission success with requestId: {}", requestId);
+
+        return ApiResponse.success(response, "Delete permission success");
+
+    }
+
+    @PostMapping("/restore")
+    public ApiResponse<String> restorePermission(@RequestBody GeneralRequest request) {
+        String requestId = UUID.randomUUID().toString();
+        log.info("Restoring permission with initiated requestId: {}", requestId);
+
+        String response = permissionService.restorePermission(request.getRequestId(), requestId);
+
+        log.info("Restoring permission success with requestId: {}", requestId);
+
+        return ApiResponse.success(response, "Restore permission success");
+    }
+
 }
--- src/main/java/com/cnh/ies/service/user/PermissionService.java
+++ src/main/java/com/cnh/ies/service/user/PermissionService.java
@@ -1,6 +1,8 @@
 package com.cnh.ies.service.user;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
@@ -31,26 +33,105 @@ public class PermissionService {
 
             List<PermissionEntity> permissions = permissionRepo.findAll();
 
-            List<PermissionInfo> permissionInfos = permissions.stream().map(permissionMapper::mapToPermissionInfo).collect(Collectors.toList());
+            List<PermissionInfo> permissionInfos = permissions.stream().map(permissionMapper::mapToPermissionInfo)
+                    .collect(Collectors.toList());
 
             PaginationModel pagination = PaginationModel.builder()
-                .page(1)
-                .limit(10)
-                .total((long) permissions.size())
-                .totalPage(1)
-                .build();
+                    .page(1)
+                    .limit(10)
+                    .total((long) permissions.size())
+                    .totalPage(1)
+                    .build();
 
             log.info("Getting all permissions success | RequestId: {}", requestId);
-            
+
             return ListDataModel.<PermissionInfo>builder()
-                .data(permissionInfos)
-                .pagination(pagination)
-                .build();
+                    .data(permissionInfos)
+                    .pagination(pagination)
+                    .build();
 
         } catch (Exception e) {
             log.error("Error getting all permissions", e);
-            throw new ApiException(ApiException.ErrorCode.INTERNAL_ERROR, e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.value(), requestId);
+            throw new ApiException(ApiException.ErrorCode.INTERNAL_ERROR, e.getMessage(),
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(), requestId);
         }
     }
 
+    public PermissionInfo getPermissionById(String id, String requestId) {
+        try {
+            log.info("Getting permission by id: {}", id);
+
+            Optional<PermissionEntity> permission = permissionRepo.findById(UUID.fromString(id));
+
+            if (permission.isEmpty()) {
+                log.error("Permission not found with id: {}", id);
+                throw new ApiException(ApiException.ErrorCode.NOT_FOUND, "Permission not found with id: " + id,
+                        HttpStatus.NOT_FOUND.value(), requestId);
+            }
+
+            if (permission.get().getIsDeleted()) {
+                log.error("Permission is deleted with id: {}", id);
+                throw new ApiException(ApiException.ErrorCode.NOT_FOUND, "Permission is deleted with id: " + id,
+                        HttpStatus.NOT_FOUND.value(), requestId);
+            }
+
+            log.info("Permission fetched successfully with id: {}", id);
+
+            return permissionMapper.mapToPermissionInfo(permission.get());
+        } catch (Exception e) {
+            log.error("Error getting permission by id", e);
+            throw new ApiException(ApiException.ErrorCode.INTERNAL_ERROR, "Error getting permission by id",
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(), requestId);
+        }
+    }
+
+    public String deletePermission(String id, String requestId) {
+        try {
+            log.info("Deleting permission with id: {}", id);
+
+            Optional<PermissionEntity> permission = permissionRepo.findById(UUID.fromString(id));
+
+            if (permission.isEmpty()) {
+                log.error("Permission not found with id: {}", id);
+                throw new ApiException(ApiException.ErrorCode.NOT_FOUND, "Permission not found with id: " + id,
+                        HttpStatus.NOT_FOUND.value(), requestId);
+            }
+
+            permission.get().setIsDeleted(true);
+            permission.get().setCode(permission.get().getCode() + "_" + "DELETED" + "_" + requestId);
+            permissionRepo.save(permission.get());
+
+            log.info("Permission deleted successfully with id: {}", id);
+
+            return "Permission deleted successfully";
+        } catch (Exception e) {
+            log.error("Error deleting permission", e);
+            throw new ApiException(ApiException.ErrorCode.INTERNAL_ERROR, "Error deleting permission",
+                    HttpStatus.INTERNAL_SERVER_ERROR.value(), requestId);
+        }
+    }
+
+    public String restorePermission(String id, String requestId) {
+        try {
+            log.info("Restoring permission with id: {}", id);
+
+        Optional<PermissionEntity> permission = permissionRepo.findById(UUID.fromString(id));
+
+        if (permission.isEmpty()) {
+            log.error("Permission not found with id: {}", id);
+            throw new ApiException(ApiException.ErrorCode.NOT_FOUND, "Permission not found with id: " + id, HttpStatus.NOT_FOUND.value(), requestId);
+        }
+        
+        permission.get().setCode(permission.get().getCode().replace("_DELETED_" + requestId, ""));
+        permission.get().setIsDeleted(false);
+        permissionRepo.save(permission.get());
+        
+        log.info("Permission restored successfully with id: {}", id);
+
+        return "Permission restored successfully";
+        } catch (Exception e) {
+            log.error("Error restoring permission", e);
+            throw new ApiException(ApiException.ErrorCode.INTERNAL_ERROR, "Error restoring permission", HttpStatus.INTERNAL_SERVER_ERROR.value(), requestId);
+        }
+    }
 }
